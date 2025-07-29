ed83ebbf5290b4e8e9956f841d372f80a4e655b2
Ifeanyi Chidoka <ik.chidoka@gmail.com>
2025-07-29 17:51:52 +0100

    downgraded tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Open Sans', 'sans-serif'],
+        ui: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        primary: 'var(--primary)',
+        accent: 'var(--accent)',
+        success: 'var(--success)',
+        warning: 'var(--warning)',
+        danger: 'var(--danger)',
+        graybase: 'var(--gray-base)',
+      },
+    },
+  },
+  plugins: [],
+};
