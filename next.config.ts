892b1bce6f38dbd73ff6ecb5fa1d9c9e1429f512
ifeanyi chidoka <ik.chidoka@gmail.com>
2025-08-12 11:33:00 +0100

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+
+const isCI = process.env.CI === 'true';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  eslint: { ignoreDuringBuilds: isCI },
+  typescript: { ignoreBuildErrors: isCI },
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
+      { protocol: 'https', hostname: 'images.unsplash.com' }
+    ]
+  },
+  experimental: {
+    typedRoutes: true
+  }
 };
 
 export default nextConfig;
