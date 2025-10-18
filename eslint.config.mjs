111f23c3cdf011a4c84ae3fd3e84a978700603d1
ifeanyi chidoka <ik.chidoka@gmail.com>
2025-10-18 13:45:23 +0100

    update eslint error on vercel

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,15 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+export default [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_" }],
+      "@typescript-eslint/ban-ts-comment": ["warn", { "ts-expect-error": "allow-with-description" }],
+      "react-hooks/exhaustive-deps": "warn",
+      "@next/next/no-img-element": "off"
+    }
+  }
 ];
-
-export default eslintConfig;
